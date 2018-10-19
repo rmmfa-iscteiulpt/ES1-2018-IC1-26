99711cafaf6671052e8d2f9a7adcd13709422d8b
fdtmg@iscte-iul.pt <Utilizador@172.17.7.190>
2018-10-19 10:21:21 +0100

    Added comments to each function for later JDoc generation

--- Engenharia Software/src/engenhariaSoftware/common/Tweet.java
+++ Engenharia Software/src/engenhariaSoftware/common/Tweet.java
@@ -9,28 +9,62 @@ public class Tweet {
 	private long id;
 	private Date createdAt;
 	
+	/**
+	 * Tweet constructor
+	 * @param user
+	 * @param text
+	 * @param id
+	 * @param createdAt
+	 */
+	
+	
 	public Tweet(String user, String text, long id, Date createdAt) {
 		this.user = user;
 		this.text = text;
 		this.id = id;
 		this.createdAt = createdAt;
 	}
+	
+	/**
+	 * Getter for user name
+	 * @return
+	 */
 
 	public String getUserName() {
 		return this.user;
 	}
 	
+	/**
+	 * Getter for the tweet text
+	 * @return
+	 */
+	
 	public String getText() {
 		return this.text;
 	}
+	
+	/**
+	 * Getter for the tweet id
+	 * @return
+	 */
 	
 	public long getId() {
 		return this.id;
 	}
 	
-	public String confirmTweet () {
+	/**
+	 * Getter for the tweet header (user name + date of creation)
+	 * @return
+	 */
+	
+	public String tweetHeader () {
 		return this.user + ": " + this.createdAt;
 	}
+	
+	/**
+	 * Getter for the date of creation
+	 * @return
+	 */
 	
 	public Date getCreatedAt() {
 		return this.createdAt;
